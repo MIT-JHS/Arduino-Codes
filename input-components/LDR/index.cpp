0be0989e6f86afa834366629d3eeca6a09810229
Aldhan (He/Him) <aldhaneka@gmail.com>
2021-03-09 17:03:28 +0700

    Create index.cpp

---
+++ input-components/LDR/index.cpp
@@ -0,0 +1,16 @@
+#define LDR A1 // define adalah keyword konstanta, yg dimana anda tidak bisa mengganti nilainya | define is a keyword which is a constant variable
+
+void setup()
+{
+  /*
+   * @params (int speed, config) https://www.arduino.cc/reference/en/language/functions/communication/serial/begin/
+   */
+	Serial.begin(9600); 
+}
+
+void loop()
+{
+  
+  int intensitas = analogRead(LDR); // mengeluarkan nilai integer rentang 366 - 1018 | returns an integer value; 366 - 1018
+	Serial.println(intensitas); // log out
+}
